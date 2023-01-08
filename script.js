817ab6f9d0a1854286d2e5d3085db14d7a537aeb
DeivideSantos <deivid_santista@hotmail.com>
2023-01-08 19:51:05 -0300

    inde.html

---
+++ script.js
@@ -0,0 +1,35 @@
+const quadrado = document.querySelector('.quadrado')
+const div = document.createElement('div')
+const div2 = document.createElement('div')
+const reiniciar = document.querySelector('.resetar')
+
+//div.classList.add('cubo')
+//quadrado.appendChild(div)
+
+
+function gradeHorizontal() {
+const grade = 437
+for (i = 0; i < grade; i++) {
+    if (i < grade) { 
+const div = document.createElement('div');
+div.classList.add('cubo')
+quadrado.appendChild(div)
+
+div.addEventListener('mousedown', function(e) {
+    div.style.background = ('black');
+
+reiniciar.addEventListener('click', function(e){
+    div.style.background = ('white');
+})
+  })
+}}
+
+
+}
+gradeHorizontal()
+
+function reset() {
+    reiniciar.addEventListener('click', function(e){
+        div.style.background = ('white')
+    })
+}
